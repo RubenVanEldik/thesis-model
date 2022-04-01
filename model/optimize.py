a099c6c2aaa733b77e802c0a55c02fa41e6304dd
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-04-01 22:11:29 +0200

    Refactor the optimization function

--- model/optimize.py
+++ model/optimize.py
@@ -42,43 +42,26 @@ def get_climate_zones(year, bidding_zone):
     }
 
 
-def sum_all_climate_zones(climate_zones, *, func=None):
-    """
-    Return the sum of all climate zones, if a function is defined it will be used to calculate the value
-    """
-    assert validate.is_climate_zone_dict(climate_zones)
-    assert validate.is_func(func, required=False)
-
-    if func is None:
-        return gp.quicksum(climate_zone for climate_zone in climate_zones.values())
-    return gp.quicksum(func(climate_zone, column) for column, climate_zone in climate_zones.items())
-
-
-def retrieve_variables(model, variables):
-    """
-    Retrieve the value of the variables after the model has been run
-    """
-    assert validate.is_model(model)
-    assert validate.is_variable_tupledict(variables)
-
-    return model.getAttr("x", variables).values()
-
-
-def create_demand_constraint(data, capacity):
+def create_demand_constraint(row, model):
     """
     Return an object with the climate zone names for wind and PV
     """
-    assert validate.is_data_row(data)
-    assert all(validate.is_climate_zone_dict(x) for x in capacity.values())
+    total_production = 0
+    for column_name in row.index:
+        if column_name.startswith("production_"):
+            total_production += row[column_name]
 
-    calculate_output = lambda capacity, climate_zone: capacity * data[climate_zone]
+    model.addConstr(total_production - row.net_storage_flow_MWh >= row.demand_MWh)
 
-    pv_production = sum_all_climate_zones(capacity["pv"], func=calculate_output)
-    onshore_production = sum_all_climate_zones(capacity["onshore"], func=calculate_output)
-    offshore_production = sum_all_climate_zones(capacity["offshore"], func=calculate_output)
-    total_production = pv_production + onshore_production + offshore_production
 
-    return total_production - data.net_storage_flow >= data.demand_MWh
+def calculate_hourly_production(row, capacities):
+    """
+    Return the production in a specific hour for a specific technology
+    """
+    total_production = 0
+    for climate_zone, capacity in capacities.items():
+        total_production += row[climate_zone] * capacity
+    return total_production
 
 
 def run(year, countries, date_range):
@@ -97,25 +80,29 @@ def run(year, countries, date_range):
             model = gp.Model("Name")
 
             """
-            Step 2: Define production variables
+            Step 2: Create an hourly_data and hourly_results DataFrame
             """
             hourly_data = get_hourly_data(year, bidding_zone, range=date_range)
+            hourly_results = hourly_data[["demand_MWh"]]
+
+            """
+            Step 3: Define production capacity variables
+            """
+            production_capacity = {}
             climate_zones = get_climate_zones(year, bidding_zone)
-
-            # Add production capacity variables
-            capacity_per_technology = {
-                "pv": model.addVars(climate_zones["pv"]),
-                "onshore": model.addVars(climate_zones["onshore"]),
-                "offshore": model.addVars(climate_zones["offshore"]),
-            }
+            for production_technology in technologies.technology_types("production"):
+                capacity = model.addVars(climate_zones[production_technology])
+                capacity_sum = gp.quicksum(capacity.values())
+                production_capacity[production_technology] = capacity_sum
+                hourly_results[f"production_{production_technology}_MWh"] = hourly_data.apply(calculate_hourly_production, args=(capacity,), axis=1)
 
             """
-            Step 3: Define storage variables and constraints
+            Step 4: Define storage variables and constraints
             """
-            # Create an object to save the storage capacity (energy & power)
-            # and add a column to the DataFrame to store the hourly net storage flow
+            # Create an object to save the storage capacity (energy & power) and add 2 columns to the results DataFrame
             storage_capacity = {}
-            hourly_data["net_storage_flow"] = 0
+            hourly_results["net_storage_flow_MWh"] = 0
+            hourly_results["energy_stored_MWh"] = 0
             # Add the variables and constraints for all storage technologies
             for storage_technology in technologies.technology_types("storage"):
                 # Get the specific storage assumptions
@@ -144,11 +131,12 @@ def run(year, countries, date_range):
                     power_capacity = storage_capacity[storage_technology]["power"]
 
                     # Get the previous state of charge and one-way efficiency
+                    soc_current = soc[timestamp]
                     soc_previous = soc.get(previous_timestamp, assumptions["soc0"])
                     efficiency = assumptions["roundtrip_efficiency"] ** 0.5
 
                     # Add the state of charge constraints
-                    model.addConstr(soc[timestamp] * energy_capacity == soc_previous * energy_capacity + (inflow[timestamp] * efficiency - outflow[timestamp] / efficiency))
+                    model.addConstr(soc_current * energy_capacity == soc_previous * energy_capacity + (inflow[timestamp] * efficiency - outflow[timestamp] / efficiency))
 
                     # Add the power capacity constraints
                     model.addConstr(inflow[timestamp] <= power_capacity)
@@ -156,31 +144,26 @@ def run(year, countries, date_range):
 
                     # Add the net flow to the total net storage
                     net_flow = inflow[timestamp] - outflow[timestamp]
-                    hourly_data.loc[timestamp, "net_storage_flow"] += net_flow
+                    hourly_results.loc[timestamp, "net_storage_flow_MWh"] += net_flow
+                    hourly_results.loc[timestamp, "energy_stored_MWh"] += soc_current * energy_capacity
 
                     # Update the previous_timestamp
                     previous_timestamp = timestamp
 
             """
-            Step 4: Define demand constraints
+            Step 5: Define demand constraints
             """
             with st.spinner("Adding demand constraints"):
-                model.addConstrs(create_demand_constraint(hourly_data.loc[timestamp], capacity_per_technology) for timestamp in hourly_data.index)
+                hourly_results.apply(create_demand_constraint, args=(model,), axis=1)
 
             """
-            Step 5: Set objective function
+            Step 6: Set objective function
             """
-            production_capacity = {
-                "pv": sum_all_climate_zones(capacity_per_technology["pv"]),
-                "onshore": sum_all_climate_zones(capacity_per_technology["onshore"]),
-                "offshore": sum_all_climate_zones(capacity_per_technology["offshore"]),
-            }
-
             obj = lcoe.calculate(production_capacity, storage_capacity, hourly_data.demand_MWh)
             model.setObjective(obj, gp.GRB.MINIMIZE)
 
             """
-            Step 6: Solve model
+            Step 7: Solve model
             """
             with st.spinner(f"Optimizing for {bidding_zone}"):
                 start_optimizing = datetime.now()
@@ -200,16 +183,23 @@ def run(year, countries, date_range):
                     return
 
             """
-            Step 7: Get the final values of the variables
+            Step 8: Get the final values for all cells in the hourly results DataFrame
+            """
+            for column_name in hourly_results:
+                if hourly_results[column_name].dtype == "object":
+                    hourly_results[column_name] = hourly_results[column_name].apply(lambda x: x.getValue())
+
+            """
+            Step 9: Get the final values of the variables
             """
             final_lcoe = model.getObjective().getValue()
-            installed_pv = sum(retrieve_variables(model, capacity_per_technology["pv"]))
-            installed_onshore = sum(retrieve_variables(model, capacity_per_technology["onshore"]))
-            installed_offshore = sum(retrieve_variables(model, capacity_per_technology["offshore"]))
+            installed_pv = production_capacity["pv"].getValue()
+            installed_onshore = production_capacity["onshore"].getValue()
+            installed_offshore = production_capacity["offshore"].getValue()
             installed_lion = storage_capacity["lion"]["energy"].X
 
             """
-            Step 8: Show results
+            Step 10: Show results
             """
             st.subheader("Key performance indicators")
             col1, col2, col3 = st.columns(3)
@@ -217,7 +207,7 @@ def run(year, countries, date_range):
             col2.metric("Firm kWh premium", "-")
             col3.metric("Curtailment", "-")
 
-            st.subheader("Generation capacity")
+            st.subheader("Production capacity")
             col1, col2, col3 = st.columns(3)
             col1.metric("Solar PV", f"{int(installed_pv / 1000):,}GW")
             col2.metric("Onshore wind", f"{int(installed_onshore / 1000):,}GW")
