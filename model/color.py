1d34c65a8268ca7e9f541dc1f37ad7c782cf6136
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-04-12 22:21:51 +0200

    Create a color module

---
+++ model/color.py
@@ -0,0 +1,46 @@
+def gray(value):
+    if value == 50:
+        return "#FAFAFA"
+    if value == 100:
+        return "#F5F5F5"
+    if value == 200:
+        return "#E5E5E5"
+    if value == 300:
+        return "#D4D4D4"
+    if value == 400:
+        return "#A3A3A3"
+    if value == 500:
+        return "#737373"
+    if value == 600:
+        return "#525252"
+    if value == 700:
+        return "#404040"
+    if value == 800:
+        return "#262626"
+    if value == 900:
+        return "#171717"
+    return "#B91C1C"  # Red
+
+
+def blue(value):
+    if value == 50:
+        return "#F0F9FF"
+    if value == 100:
+        return "#E0F2FE"
+    if value == 200:
+        return "#BAE6FD"
+    if value == 300:
+        return "#7DD3FC"
+    if value == 400:
+        return "#38BDF8"
+    if value == 500:
+        return "#0EA5E9"
+    if value == 600:
+        return "#0284C7"
+    if value == 700:
+        return "#0369A1"
+    if value == 800:
+        return "#075985"
+    if value == 900:
+        return "#0C4A6E"
+    return "#B91C1C"  # Red
