4d98b30dee966b4158ad56e67916a6e8237e909f
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-05-08 16:39:22 +0200

    Set the analyze methods in the right order

--- model/analyze.py
+++ model/analyze.py
@@ -109,41 +109,6 @@ def _get_hourly_results(run_name, *, group=None):
         return total_hourly_results
 
 
-def hourly_results(run_name):
-    """
-    Show the hourly results in a chart and table
-    """
-    assert validate.is_string(run_name)
-
-    st.title("🕰️ Hourly results")
-
-    # Get hourly results for a country
-    all_hourly_results = _get_hourly_results(run_name, group="country")
-    config = utils.read_yaml(f"../output/{run_name}/config.yaml")
-    country = st.selectbox("Country", config["countries"], format_func=lambda country: country["name"])
-    hourly_results = all_hourly_results[country["nuts_2"]]
-
-    # Filter the data columns
-    hourly_results.columns = [utils.format_column_name(column_name) for column_name in hourly_results.columns]
-    columns = st.multiselect("Columns", hourly_results.columns)
-    hourly_results = hourly_results[columns]
-
-    # Show the chart and DataFrame if any columns are selected
-    if columns:
-        # Filter the data temporarily
-        start_data = hourly_results.index.min().to_pydatetime()
-        end_data = hourly_results.index.max().to_pydatetime()
-        data_range = st.slider("Date range", value=(start_data, end_data), min_value=start_data, max_value=end_data)
-        hourly_results = hourly_results.loc[data_range[0] : data_range[1]]
-
-        # Show the line chart
-        st.line_chart(hourly_results)
-
-        # Show the table in an expander
-        with st.expander("Raw data"):
-            st.dataframe(hourly_results)
-
-
 def statistics(run_name):
     """
     Show the key indicators for a run
@@ -184,6 +149,41 @@ def statistics(run_name):
     for index, technology in enumerate(total_storage_capacity_energy.index):
         installed_hours = total_storage_capacity_energy[technology] / total_hourly_results.demand_MWh.mean()
         cols[index].metric(technologies.labelize(technology[0]), f"{installed_hours:.1f}Hr")
+
+
+def hourly_results(run_name):
+    """
+    Show the hourly results in a chart and table
+    """
+    assert validate.is_string(run_name)
+
+    st.title("🕰️ Hourly results")
+
+    # Get hourly results for a country
+    all_hourly_results = _get_hourly_results(run_name, group="country")
+    config = utils.read_yaml(f"../output/{run_name}/config.yaml")
+    country = st.selectbox("Country", config["countries"], format_func=lambda country: country["name"])
+    hourly_results = all_hourly_results[country["nuts_2"]]
+
+    # Filter the data columns
+    hourly_results.columns = [utils.format_column_name(column_name) for column_name in hourly_results.columns]
+    columns = st.multiselect("Columns", hourly_results.columns)
+    hourly_results = hourly_results[columns]
+
+    # Show the chart and DataFrame if any columns are selected
+    if columns:
+        # Filter the data temporarily
+        start_data = hourly_results.index.min().to_pydatetime()
+        end_data = hourly_results.index.max().to_pydatetime()
+        data_range = st.slider("Date range", value=(start_data, end_data), min_value=start_data, max_value=end_data)
+        hourly_results = hourly_results.loc[data_range[0] : data_range[1]]
+
+        # Show the line chart
+        st.line_chart(hourly_results)
+
+        # Show the table in an expander
+        with st.expander("Raw data"):
+            st.dataframe(hourly_results)
 
 
 def countries(run_name):
