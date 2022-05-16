5ff4e8cdf22710ee59e054aec3822f14176262ec
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-05-16 10:52:14 +0200

    Format the abbreviations in uppercase

--- model/utils/format_str.py
+++ model/utils/format_str.py
@@ -6,8 +6,16 @@ import validate
 @st.experimental_memo(show_spinner=False)
 def format_str(str):
     """
-    Replace underscores with spaces and capitalize the string
+    Replace underscores with spaces, capitalize the string, and convert the abbreviations into uppercase
     """
     assert validate.is_string(str)
 
-    return str.replace("_", " ").capitalize()
+    new_str = str.replace("_", " ").capitalize()
+
+    # Format the abbreviations in uppercase
+    abbreviations = ["lcoe", "hvac", "hvdc"]
+    for abbreviation in abbreviations:
+        new_str = new_str.replace(abbreviation, abbreviation.upper())
+        new_str = new_str.replace(abbreviation.capitalize(), abbreviation.upper())
+
+    return new_str
