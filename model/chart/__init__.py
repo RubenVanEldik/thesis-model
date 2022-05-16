d8c6ecc0c145832fe03dbf0f1a4eb46e3aba1398
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-05-16 12:12:31 +0200

    Move the chart classes to their own folder

---
+++ model/chart/__init__.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+
+from .chart import Chart
+from .map import Map
+
+# Set the font for all plots to serif
+plt.rcParams["font.family"] = "serif"
--- model/chart/chart.py
+++ model/chart/chart.py
@@ -1,16 +1,6 @@
 from matplotlib import pyplot as plt
 from matplotlib import ticker as mticker
-import pandas as pd
-import re
 import streamlit as st
-import numpy as np
-
-import colors
-import validate
-import utils
-
-# Set the font for all plots to serif
-plt.rcParams["font.family"] = "serif"
 
 
 class Chart:
@@ -51,36 +41,3 @@ class Chart:
 
     def save(self, filepath):
         plt.savefig(filepath, dpi=250, bbox_inches="tight", pad_inches=0.2)
-
-
-class Map:
-    """
-    Create a geopandas map based on a Series with country level data
-    """
-
-    fig = None
-    ax = None
-
-    def __init__(self, data, *, label=None, format_percentage=False):
-        # Create the figure
-        self.fig, self.ax = plt.subplots(figsize=(7, 5))
-        self.ax.axis("off")
-        self.ax.margins(0.02)
-
-        # Create the color bar
-        colormap = colors.colormap("blue")
-        vmin = data.min()
-        vmax = data[data < np.Inf].max()
-        scalar_mappable = plt.cm.ScalarMappable(cmap=colormap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
-        format = lambda x, pos: f"{x:.0%}" if format_percentage else x
-        self.fig.colorbar(scalar_mappable, shrink=0.7, aspect=20, label=label, format=format)
-
-        # Get the geopandas DataFrame and map the data to it
-        map_df = utils.get_geometries_of_countries(data.index)
-        map_df["data"] = map_df.index.map(data)
-
-        # Plot the data
-        map_df.plot(column="data", cmap=colormap, linewidth=0.5, ax=self.ax, edgecolor=colors.get("gray", 600))
-
-    def save(self, filepath):
-        plt.savefig(filepath, dpi=2500, bbox_inches="tight", pad_inches=0.2)
