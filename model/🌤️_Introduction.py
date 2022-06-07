1578ebed7c2cd5d63e81575924fb1cd72f4975b6
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-06-07 13:57:20 +0200

    Update the introduction text

--- model/🌤️_Introduction.py
+++ model/🌤️_Introduction.py
@@ -7,7 +7,7 @@ st.markdown(
     """
     ## Welcome!
 
-    This is an optimization model for a fully intermittent renewable Pan-European electricity grid. This model allows you to change the scopen in the geographical, temporal and technological domain.
+    This is an optimization model for a fully intermittent renewable Pan-European electricity grid. The model aims to find the least-cost solution for the deployment of solar PV, wind energy and storage. By overbuilding generation capacity to some degree, significant less storage capacity is required, resulting in lower system costs.
 
     The demand and climate data used in this model are from ENTSO-E's 2021 European Resource Adequacy Assessment (ERAA) and can be downloaded [here](https://www.entsoe.eu/outlooks/eraa/2021/eraa-downloads/). The techno-economic data is from NREL's Annual Technology Baseline and can be found [here](https://atb.nrel.gov/).
 
