dc9a9b3fa62c5d553bf67934472e177ab9e3a57f
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-06-10 10:17:47 +0200

    Improve the validation functions

--- model/validate.py
+++ model/validate.py
@@ -18,9 +18,10 @@ def is_bidding_zone_list(value, *, required=True):
     if value is None:
         return not required
 
-    is_list = type(value) is list
-    has_bidding_zone_items = all(is_bidding_zone(x) for x in value)
-    return is_list and has_bidding_zone_items
+    if type(value) is not list:
+        return False
+
+    return all(is_bidding_zone(x) for x in value)
 
 
 def is_bool(value, *, required=True):
@@ -41,8 +42,11 @@ def is_climate_zone_dict(value, *, required=True):
     if value is None:
         return not required
 
-    is_tupledict = isinstance(value, gurobipy.tupledict)
-    has_valid_keys = all(is_climate_zone(x) for x in value.keys())
+    if not isinstance(value, gurobipy.tupledict):
+        return False
+    if not all(is_climate_zone(x) for x in value.keys()):
+        return False
+
     return is_tupledict and has_valid_keys
 
 
@@ -65,16 +69,24 @@ def is_config(value, *, required=True, new_config=False):
         return not required
 
     if type(value) is not dict:
-        return false
+        return False
 
-    has_valid_model_year = is_model_year(value["model_year"])
-    has_valid_countries = is_country_obj_list(value["countries"])
-    has_valid_date_range = is_date_range(value["date_range"])
-    has_valid_time_limit = is_datetime(value["optimization"]["time_limit"]) and (not new_config or value["optimization"]["time_limit"] > datetime.datetime.now())
-    has_valid_method = is_integer(value["optimization"]["method"], min_value=-1, max_value=6)
-    has_valid_thread_count = is_integer(value["optimization"]["thread_count"], min_value=1)
+    if not is_model_year(value["model_year"]):
+        return False
+    if not is_country_obj_list(value["countries"]):
+        return False
+    if not is_date_range(value["date_range"]):
+        return False
+    if not is_datetime(value["optimization"]["time_limit"]):
+        return False
+    if new_config and value["optimization"]["time_limit"] < datetime.datetime.now():
+        return False
+    if not is_integer(value["optimization"]["method"], min_value=-1, max_value=6):
+        return False
+    if not is_integer(value["optimization"]["thread_count"], min_value=1):
+        return False
 
-    return has_valid_model_year and has_valid_countries and has_valid_date_range and has_valid_time_limit and has_valid_method
+    return True
 
 
 def is_country_code(value, *, required=True, type):
@@ -92,48 +104,53 @@ def is_country_code_list(value, *, required=True, type):
     if value is None:
         return not required
 
-    is_list = is_list_like(value)
-    has_valid_items = all(is_country_code(code, type=type) for code in value)
-    return is_list and has_valid_items
+    if not is_list_like(value):
+        return False
+
+    return all(is_country_code(code, type=type) for code in value)
 
 
 def is_country_obj(value, *, required=True):
     if value is None:
         return not required
 
-    is_dict = type(value) is dict
-    has_required_keys = bool(value["name"] and value["bidding_zones"])
-    return is_dict and has_required_keys
+    if not type(value) is dict:
+        return False
+
+    return bool(value["name"] and value["bidding_zones"])
 
 
 def is_country_obj_list(value, *, required=True):
     if value is None:
         return not required
 
-    is_list = type(value) is list
-    has_items = len(value) > 0
-    has_valid_items = all(is_country_obj(x) for x in value)
-    return is_list and has_items and has_valid_items
+    if not is_list_like(value) or not len(value) > 0:
+        return False
+
+    return all(is_country_obj(x) for x in value)
 
 
 def is_dataframe(value, *, required=True, column_validator=None):
     if value is None:
         return not required
 
-    if column_validator and not all(column_validator(column_name) for column_name in value.columns):
+    if not isinstance(value, pd.DataFrame):
         return False
 
-    return isinstance(value, pd.DataFrame)
+    if column_validator:
+        return all(column_validator(column_name) for column_name in value.columns)
+
+    return True
 
 
 def is_dataframe_dict(value, *, required=True):
     if value is None:
         return not required
 
-    is_dict = type(value) is dict
-    has_valid_items = all(is_dataframe(value[x]) for x in value)
+    if not type(value) is dict:
+        return False
 
-    return is_dict and has_valid_items
+    return all(is_dataframe(value[x]) for x in value)
 
 
 def is_date(value, *, required=True):
@@ -170,8 +187,7 @@ def is_directory_path(value, *, required=True):
     if value is None:
         return not required
 
-    is_valid_path = bool(re.search("^[0-9a-zA-Z_/\.\(\) ]+$", value))
-    return is_valid_path
+    return bool(re.search("^[0-9a-zA-Z_/\.\(\) ]+$", value))
 
 
 def is_aggregation_level(value, *, required=True):
@@ -185,32 +201,38 @@ def is_hourly_data_row(value, *, required=True):
     if value is None:
         return not required
 
-    is_series = is_series(value)
+    if not is_series(value):
+        return False
+
     includes_demand = any(x.startswith("demand_") for x in value.index)
     includes_pv = any(x.startswith("pv_") for x in value.index)
     includes_onshore = any(x.startswith("onshore_") for x in value.index)
-    return is_series and includes_demand and includes_pv and includes_onshore
+    return includes_demand and includes_pv and includes_onshore
 
 
 def is_hourly_results_row(value, *, required=True):
     if value is None:
         return not required
 
-    is_series = is_series(value)
+    if not is_series(value):
+        return False
+
     includes_demand = any(x.startswith("demand_") for x in value.index)
     includes_production = any(x.startswith("production_") for x in value.index)
     includes_net_storage_flow = any(x.startswith("net_storage_flow") for x in value.index)
-    return is_series and includes_demand and includes_production and includes_net_storage_flow
+    return includes_demand and includes_production and includes_net_storage_flow
 
 
 def is_date_range(value, *, required=True):
     if value is None:
         return not required
 
-    is_dict = type(value) is dict
+    if not type(value) is dict:
+        return False
+
     has_valid_start_date = value["start"] and type(value["start"]) is datetime.date
     has_valid_end_date = value["end"] and type(value["end"]) is datetime.date
-    return is_dict and has_valid_start_date and has_valid_end_date
+    return has_valid_start_date and has_valid_end_date
 
 
 def is_filepath(value, *, required=True, suffix=None):
@@ -226,19 +248,26 @@ def is_filepath_list(value, *, required=True, suffix=None):
     if value is None:
         return not required
 
-    is_list = is_list_like(value)
-    has_valid_items = all(is_filepath(filepath, suffix=suffix) for filepath in value)
+    if not is_list_like(value):
+        return False
 
-    return is_list and has_valid_items
+    return all(is_filepath(filepath, suffix=suffix) for filepath in value)
 
 
 def is_float(value, *, required=True, min_value=None, max_value=None):
     if value is None:
         return not required
 
-    is_float = type(value) is float
-    valid_float = (min_value is None or value >= min_value) and (max_value is None or value <= max_value)
-    return is_float and valid_float
+    if not type(value) is float:
+        return False
+
+    if min_value is not None and value < min_value:
+        return False
+
+    if max_value is not None and value > max_value:
+        return False
+
+    return True
 
 
 def is_func(value, *, required=True):
@@ -276,9 +305,16 @@ def is_integer(value, *, required=True, min_value=None, max_value=None):
     if value is None:
         return not required
 
-    is_integer = type(value) is int
-    valid_integer = (min_value is None or value >= min_value) and (max_value is None or value <= max_value)
-    return is_integer and valid_integer
+    if not type(value) is int:
+        return False
+
+    if min_value is not None and value < min_value:
+        return False
+
+    if max_value is not None and value > max_value:
+        return False
+
+    return True
 
 
 def is_list_like(value, *, required=True):
@@ -320,11 +356,12 @@ def is_sensitivity_config(value, *, required=True):
     if value is None:
         return not required
 
-    is_dict = type(value) is dict
+    if not type(value) is dict:
+        return False
+
     has_valid_steps = all(type(key) is str and type(value) is float for key, value in value["steps"].items())
     has_valid_variables = len(value["variables"]) and all(type(variable) is str for variable in value["variables"])
-
-    return is_dict and has_valid_steps and has_valid_variables
+    return has_valid_steps and has_valid_variables
 
 
 def is_series(value, *, required=True):
@@ -338,10 +375,10 @@ def is_string(value, *, required=True, min_length=0):
     if value is None:
         return not required
 
-    is_string = type(value) is str
-    has_valid_length = len(value) >= min_length
+    if not type(value) is str:
+        return False
 
-    return is_string and has_valid_length
+    return len(value) >= min_length
 
 
 def is_technology(value, *, required=True):
@@ -386,6 +423,7 @@ def is_variable_tupledict(value, *, required=True):
     if value is None:
         return not required
 
-    is_tupledict = isinstance(value, gurobipy.tupledict)
-    has_valid_values = all(is_variable(x) for x in value.values())
-    return is_tupledict and has_valid_values
+    if not isinstance(value, gurobipy.tupledict):
+        return False
+
+    return all(is_variable(x) for x in value.values())
