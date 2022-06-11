727a193b761d1188bc4a465528f047fcbeb7ad19
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-06-11 12:49:11 +0200

    Change the name of the analyze module to 'analysis'

--- model/pages/2_📈_Analysis.py
+++ model/pages/2_📈_Analysis.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 
-import analyze
+import analysis
 import utils
 
 # Select the run to analyze
@@ -10,11 +10,11 @@ selected_run = st.sidebar.selectbox("Previous runs", previous_runs)
 
 # Select the type of analysis
 if os.path.isfile(f"./output/{selected_run}/sensitivity.yaml"):
-    analysis = "sensitivity"
+    analysis_type = "sensitivity"
 else:
-    analysis_options = ["statistics", "hourly_results", "countries", "correlation", "duration_curve"]
-    analysis = st.sidebar.radio("Type of analysis", analysis_options, format_func=utils.format_str)
+    analysis_type_options = ["statistics", "hourly_results", "countries", "correlation", "duration_curve"]
+    analysis_type = st.sidebar.radio("Type of analysis", analysis_type_options, format_func=utils.format_str)
 
 # Run the analysis
 st.sidebar.header("Options")
-getattr(analyze, analysis)(selected_run)
+getattr(analysis, analysis_type)(selected_run)
