8eca67b6869042b79d2628181c4ec48d8bb95bc5
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-06-19 16:17:56 +0200

    Update the production technology status message per timestamp

--- model/optimization/optimize.py
+++ model/optimization/optimize.py
@@ -85,8 +85,6 @@ def optimize(config, *, resolution, previous_resolution, status, output_folder):
         """
         temporal_results[bidding_zone]["production_total_MW"] = 0
         for production_technology in config["technologies"]["production"]:
-            status.update(f"Adding {utils.labelize_technology(production_technology, capitalize=False)} production to {bidding_zone}")
-
             # Create a capacity variable for each climate zone
             climate_zones = [re.match(f"{production_technology}_(.+)_cf", column).group(1) for column in temporal_data[bidding_zone].columns if column.startswith(f"{production_technology}_")]
             if previous_resolution:
@@ -101,6 +99,7 @@ def optimize(config, *, resolution, previous_resolution, status, output_folder):
 
             # Calculate the temporal production for a specific technology
             def calculate_production_in_row(row, production_technology, capacities):
+                status.update(f"Adding {utils.labelize_technology(production_technology, capitalize=False)} production to {bidding_zone}", timestamp=row.name)
                 return sum(row[f"{production_technology}_{climate_zone}_cf"] * capacity for climate_zone, capacity in capacities.items())
 
             # Calculate the temporal production and add it to the temporal_results DataFrame
