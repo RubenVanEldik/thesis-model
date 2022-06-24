5971a67982c106dca0d5032bd297b594f5324ae5
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-06-24 09:48:05 +0200

    Show a warning and return the function if there are no previous runs to analyze

--- model/pages/2_📈_Analysis.py
+++ model/pages/2_📈_Analysis.py
@@ -4,22 +4,34 @@ import streamlit as st
 import analysis
 import utils
 
-# Select the run to analyze
-previous_runs = utils.get_previous_runs()
-selected_run = st.sidebar.selectbox("Previous runs", previous_runs)
 
-# Select the resolution to show the data of
-config = utils.read_yaml(f"./output/{selected_run}/config.yaml")
-sorted_resolution_stages = utils.get_sorted_resolution_stages(config)
-selected_resolution = st.sidebar.selectbox("Resolution", sorted_resolution_stages)
+def run():
+    # Get the previous runs
+    previous_runs = utils.get_previous_runs()
 
-# Select the type of analysis
-if os.path.isfile(f"./output/{selected_run}/sensitivity.yaml"):
-    analysis_type = "sensitivity"
-else:
-    analysis_type_options = ["statistics", "temporal_results", "countries", "correlation", "duration_curve"]
-    analysis_type = st.sidebar.radio("Type of analysis", analysis_type_options, format_func=utils.format_str)
+    # Show a warning and return the function if there are no previous runs
+    if not previous_runs:
+        st.sidebar.warning("There are no previous runs to analyze")
+        return
 
-# Run the analysis
-st.sidebar.header("Options")
-getattr(analysis, analysis_type)(selected_run, selected_resolution)
+    # Select the run to analyze
+    selected_run = st.sidebar.selectbox("Previous runs", previous_runs)
+
+    # Select the resolution to show the data of
+    config = utils.read_yaml(f"./output/{selected_run}/config.yaml")
+    sorted_resolution_stages = utils.get_sorted_resolution_stages(config)
+    selected_resolution = st.sidebar.selectbox("Resolution", sorted_resolution_stages)
+
+    # Select the type of analysis
+    if os.path.isfile(f"./output/{selected_run}/sensitivity.yaml"):
+        analysis_type = "sensitivity"
+    else:
+        analysis_type_options = ["statistics", "temporal_results", "countries", "correlation", "duration_curve"]
+        analysis_type = st.sidebar.radio("Type of analysis", analysis_type_options, format_func=utils.format_str)
+
+    # Run the analysis
+    st.sidebar.header("Options")
+    getattr(analysis, analysis_type)(selected_run, selected_resolution)
+
+
+run()
