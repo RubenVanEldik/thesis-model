8c31fa14f0198eb15e4dd06b01f2375a6fb0f71c
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-06-25 14:07:23 +0200

    Get the correct config for the sensitivity analysis

--- model/pages/2_📈_Analysis.py
+++ model/pages/2_📈_Analysis.py
@@ -17,14 +17,23 @@ def run():
 
     # Select the run to analyze
     selected_run = st.sidebar.selectbox("Previous runs", previous_completed_runs)
+    is_sensitivity_analysis = os.path.isfile(f"./output/{selected_run}/sensitivity.yaml")
+
+    # Get the config
+    if is_sensitivity_analysis:
+        # Get the config for the first step
+        sensitivity_config = utils.read_yaml(f"./output/{selected_run}/sensitivity.yaml")
+        first_step = next(iter(sensitivity_config["steps"]))
+        config = utils.read_yaml(f"./output/{selected_run}/{first_step}/config.yaml")
+    else:
+        config = utils.read_yaml(f"./output/{selected_run}/config.yaml")
 
     # Select the resolution to show the data of
-    config = utils.read_yaml(f"./output/{selected_run}/config.yaml")
     sorted_resolution_stages = utils.get_sorted_resolution_stages(config)
     selected_resolution = st.sidebar.selectbox("Resolution", sorted_resolution_stages)
 
     # Select the type of analysis
-    if os.path.isfile(f"./output/{selected_run}/sensitivity.yaml"):
+    if is_sensitivity_analysis:
         analysis_type = "sensitivity"
     else:
         analysis_type_options = ["statistics", "temporal_results", "countries", "correlation", "duration_curve"]
