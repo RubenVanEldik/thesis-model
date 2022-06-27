620a1fa9d595fec0eac62e680001598e8feca549
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-06-27 11:26:29 +0200

    Return an empty list from utils.get_previous_runs if there is no output folder

--- utils/get_previous_runs.py
+++ utils/get_previous_runs.py
@@ -6,6 +6,11 @@ def get_previous_runs():
     Get the names of all previous runs
     """
     output_folder = "./output"
+
+    # Return an empty list if there is no output folder
+    if not os.path.isdir(output_folder):
+        return []
+
     files_and_directories = os.listdir(output_folder)
     directories = [item for item in files_and_directories if os.path.isdir(os.path.join(output_folder, item))]
     return sorted(directories, reverse=True)
