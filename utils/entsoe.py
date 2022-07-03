00a5313b0812f94dadd45055cdf054a5a5914a98
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-07-03 18:34:17 +0200

    Fix the order of lines utils.entsoe

--- utils/entsoe.py
+++ utils/entsoe.py
@@ -3,10 +3,10 @@ from entsoe import EntsoePandasClient
 import os
 
 # Load the variables from the .env file
-api_key = os.getenv("ENTSOE_KEY")
+dotenv.load_dotenv(".env")
 
 # Raise an error if the API key has not been set
-dotenv.load_dotenv(".env")
+api_key = os.getenv("ENTSOE_KEY")
 if not api_key:
     raise KeyError("ENTSOE_KEY variable has not been set")
 
