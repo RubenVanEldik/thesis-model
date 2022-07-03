867722250c3cdc89793cd99d5aa20fc433c0c0da
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-07-03 18:38:09 +0200

    Only raise the key error when the ENTSO-E client is called

--- utils/entsoe_client.py
+++ utils/entsoe_client.py
@@ -5,10 +5,11 @@ import os
 # Load the variables from the .env file
 dotenv.load_dotenv(".env")
 
-# Raise an error if the API key has not been set
-api_key = os.getenv("ENTSOE_KEY")
-if not api_key:
-    raise KeyError("ENTSOE_KEY variable has not been set")
 
-# Return an initialized ENTSO-E Pandas client
-entsoe_client = EntsoePandasClient(api_key=api_key)
+def entsoe_client():
+    # Raise an error if the API key has not been set
+    api_key = os.getenv("ENTSOE_KEY")
+    if not api_key:
+        raise KeyError("ENTSOE_KEY variable has not been set")
+
+    return EntsoePandasClient(api_key=api_key)
