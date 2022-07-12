399919fcd9cefe2256c90c498ba3f811bc52cbfd
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-07-12 23:13:48 +0200

    Add a tuning bash script

---
+++ tuning/tune.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Make a new directory for this tuning run and move to it
+new_directory=$(date +%s)
+mkdir $new_directory
+cd $new_directory
+
+# Get the LP model
+run_name="Run XXXX"
+resolution="1H"
+lp_model="../../output/$run_name/$resolution/model.lp"
+lp_parameters="../../output/$run_name/$resolution/parameters.prm"
+
+# Tune
+grbtune Method=2 TuneBaseSettings="$lp_parameters" TuneTimeLimit=$(expr 1 * 60) "$lp_model"
