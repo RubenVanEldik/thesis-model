83bb2977cc5b1f31b19eb43cfca81e1d1c47c99d
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-07-26 14:52:41 +0200

    Return the ENTSO-E client directly, instead of via a function

--- utils/entsoe_client.py
+++ utils/entsoe_client.py
@@ -1,15 +1,12 @@
-import dotenv
 from entsoe import EntsoePandasClient
-import os
 
-# Load the variables from the .env file
-dotenv.load_dotenv(".env")
+from .getenv import getenv
 
 
-def entsoe_client():
-    # Raise an error if the API key has not been set
-    api_key = os.getenv("ENTSOE_KEY")
-    if not api_key:
-        raise KeyError("ENTSOE_KEY variable has not been set")
+# Raise an error if the API key has not been set
+api_key = getenv("ENTSOE_KEY")
+if not api_key:
+    raise KeyError("ENTSOE_KEY variable has not been set")
 
-    return EntsoePandasClient(api_key=api_key)
+# Initialize the entsoe_client
+entsoe_client = EntsoePandasClient(api_key=api_key)
