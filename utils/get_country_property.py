5ea11fd23c18198052a08be4a4af9dc0afb8602b
Ruben van Eldik <r.h.vaneldik@students.uu.nl>
2022-07-27 12:02:27 +0200

    Change 'type' to 'code_type' in utils.get_coutnry_property

--- utils/get_country_property.py
+++ utils/get_country_property.py
@@ -2,19 +2,19 @@ import utils
 import validate
 
 
-def get_country_property(country_code, key, *, type="nuts_2"):
+def get_country_property(country_code, key, *, code_type="nuts_2"):
     """
     Get a specific property of a country via its code
     """
-    assert validate.is_country_code_type(type)
-    assert validate.is_country_code(country_code, type=type)
+    assert validate.is_country_code_type(code_type)
+    assert validate.is_country_code(country_code, type=code_type)
     assert validate.is_string(key)
 
     # Read the countries
     countries = utils.read_yaml(utils.path("input", "countries.yaml"))
 
     # Get the specific country
-    country = next(country for country in countries if country[type] == country_code)
+    country = next(country for country in countries if country[code_type] == country_code)
 
     # Return the key from the country
     return country.get(key)
